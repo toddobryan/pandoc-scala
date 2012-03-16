ef9676b0ae0fec0d8c5b6a6e8a361ae37aa7988d
Todd O'Bryan <toddobryan@gmail.com>
2012-03-16 15:40:32 -0400

    still working on Pretty, also added Parsing - currently a mess

--- src/main/scala/pandoc/text/pretty/package.scala
+++ src/main/scala/pandoc/text/pretty/package.scala
@@ -1,14 +1,13 @@
 package pandoc.text
 
 package object pretty {
-  case class RenderState[T](
-      output: Seq[T],
+  case class RenderState(
+      output: List[String], // in reverse order
       prefix: String,
       usePrefix: Boolean,
       lineLength: Option[Int],
       column: Int,
-      newLines: Int
-  )
+      newLines: Int)
   
   sealed abstract class D {
     def isBlank: Boolean = false
@@ -50,14 +49,185 @@ package object pretty {
       Doc(this.content.reverse.dropWhile(_.isBlank).reverse)
     }
   }
+  def empty = Doc(Nil)
   
   def cat(docs: List[Doc]): Doc = Doc(docs.flatMap(_.content))
   def hcat = cat _
   def hsep(docs: List[Doc]): Doc = docs.foldRight[Doc](empty)((l: Doc, r: Doc) => l <+> r)
   def vcat(docs: List[Doc]): Doc = docs.foldRight[Doc](empty)((l: Doc, r: Doc) => l %% r)
   def vsep(docs: List[Doc]): Doc = docs.foldRight[Doc](empty)((l: Doc, r: Doc) => l %+% r)
+  
+  def outp(offset: Int, s: String, state: RenderState): RenderState = {
+    if (offset <= 0) {
+      val rawPref = state.prefix
+      if (state.column == 0 && state.usePrefix && rawPref != "") {
+        val pref: List[Char] = rawPref.toCharArray.toList.reverse.dropWhile(_.isSpaceChar).reverse
+        RenderState(pref.mkString :: state.output, state.prefix, state.usePrefix, 
+            state.lineLength, state.column + realLength(pref), state.newLines)
+      } else if (offset < 0) {
+        RenderState(s :: state.output, state.prefix, state.usePrefix, 
+            state.lineLength, 0, state.newLines + 1)
+      } else {
+        state
+      }
+    } else {
+      val pref = state.prefix
+      val statePrime: RenderState = 
+        if (state.column == 0 && state.usePrefix && pref != "") {
+    	  RenderState(pref :: state.output, state.prefix, state.usePrefix,
+    	      state.lineLength, state.column + realLength(pref.toCharArray.toList), state.newLines)
+        } else {
+          state
+        }
+      RenderState(s :: statePrime.output, statePrime.prefix, statePrime.usePrefix,
+              statePrime.lineLength, statePrime.column + offset, 0)
+    }
+  }
+  
+  def render(lineLength: Option[Int], doc: Doc): String = {
+    renderDoc(doc, RenderState(Nil, "", true, lineLength, 0, 2)).output.reverse.mkString
+  }
+  
+  def renderDoc(doc: Doc, state: RenderState): RenderState = {
+    renderList(doc.content, state)
+  }
+  
+  def renderList(elts: List[D], state: RenderState): RenderState = {
+    elts match {
+      case Nil => state
+      case Text(offset, s) :: xs => outp(offset, s, state)
+    }
+  }
 
-  def empty = Doc(Nil)
+/*
+-- | Renders a 'Doc'.  @render (Just n)@ will use
+-- a line length of @n@ to reflow text on breakable spaces.
+-- @render Nothing@ will not reflow text.
+render :: (Monoid a, IsString a)
+       => Maybe Int -> Doc -> a
+render linelen doc = fromString . mconcat . reverse . output $
+  execState (renderDoc doc) startingState
+   where startingState = RenderState{
+                            output = mempty
+                          , prefix = ""
+                          , usePrefix = True
+                          , lineLength = linelen
+                          , column = 0
+                          , newlines = 2 }
+
+renderDoc :: (IsString a, Monoid a)
+          => Doc -> DocState a
+renderDoc = renderList . toList . unDoc
+
+renderList :: (IsString a, Monoid a)
+           => [D] -> DocState a
+renderList [] = return ()
+renderList (Text off s : xs) = do
+  outp off s
+  renderList xs
+
+renderList (Prefixed pref d : xs) = do
+  st <- get
+  let oldPref = prefix st
+  put st{ prefix = prefix st ++ pref }
+  renderDoc d
+  modify $ \s -> s{ prefix = oldPref }
+  renderList xs
+
+renderList (Flush d : xs) = do
+  st <- get
+  let oldUsePrefix = usePrefix st
+  put st{ usePrefix = False }
+  renderDoc d
+  modify $ \s -> s{ usePrefix = oldUsePrefix }
+  renderList xs
+
+renderList (BeforeNonBlank d : xs) =
+  case xs of
+    (x:_) | isBlank x -> renderList xs
+          | otherwise -> renderDoc d >> renderList xs
+    []                -> renderList xs
+
+renderList (BlankLine : xs) = do
+  st <- get
+  case output st of
+     _ | newlines st > 1 || null xs -> return ()
+     _ | column st == 0 -> do
+       outp (-1) "\n"
+     _         -> do
+       outp (-1) "\n"
+       outp (-1) "\n"
+  renderList xs
+
+renderList (CarriageReturn : xs) = do
+  st <- get
+  if newlines st > 0 || null xs
+     then renderList xs
+     else do
+       outp (-1) "\n"
+       renderList xs
+
+renderList (NewLine : xs) = do
+  outp (-1) "\n"
+  renderList xs
+
+renderList (BreakingSpace : CarriageReturn : xs) = renderList (CarriageReturn:xs)
+renderList (BreakingSpace : NewLine : xs) = renderList (NewLine:xs)
+renderList (BreakingSpace : BlankLine : xs) = renderList (BlankLine:xs)
+renderList (BreakingSpace : BreakingSpace : xs) = renderList (BreakingSpace:xs)
+renderList (BreakingSpace : xs) = do
+  let isText (Text _ _)       = True
+      isText (Block _ _)      = True
+      isText _                = False
+  let isBreakingSpace BreakingSpace = True
+      isBreakingSpace _             = False
+  let xs' = dropWhile isBreakingSpace xs
+  let next = takeWhile isText xs'
+  st <- get
+  let off = sum $ map offsetOf next
+  case lineLength st of
+        Just l | column st + 1 + off > l -> do
+          outp (-1) "\n"
+          renderList xs'
+        _  -> do
+          outp 1 " "
+          renderList xs'
+
+renderList (b1@Block{} : b2@Block{} : xs) =
+  renderList (mergeBlocks False b1 b2 : xs)
+
+renderList (b1@Block{} : BreakingSpace : b2@Block{} : xs) =
+  renderList (mergeBlocks True b1 b2 : xs)
+
+renderList (Block width lns : xs) = do
+  st <- get
+  let oldPref = prefix st
+  case column st - realLength oldPref of
+        n | n > 0 -> modify $ \s -> s{ prefix = oldPref ++ replicate n ' ' }
+        _         -> return ()
+  renderDoc $ blockToDoc width lns
+  modify $ \s -> s{ prefix = oldPref }
+  renderList xs
+
+mergeBlocks :: Bool -> D -> D -> D
+mergeBlocks addSpace (Block w1 lns1) (Block w2 lns2) =
+  Block (w1 + w2 + if addSpace then 1 else 0) $
+     zipWith (\l1 l2 -> pad w1 l1 ++ l2) (lns1 ++ empties) (map sp lns2 ++ empties)
+    where empties = replicate (abs $ length lns1 - length lns2) ""
+          pad n s = s ++ replicate (n - realLength s) ' '
+          sp "" = ""
+          sp xs = if addSpace then (' ' : xs) else xs
+mergeBlocks _ _ _ = error "mergeBlocks tried on non-Block!"
+
+blockToDoc :: Int -> [String] -> Doc
+blockToDoc _ lns = text $ intercalate "\n" lns
+
+offsetOf :: D -> Int
+offsetOf (Text o _)       = o
+offsetOf (Block w _)      = w
+offsetOf BreakingSpace    = 1
+offsetOf _                = 0
+*/
   def toChunks(s: List[Char]): List[D] = {
     s match {
       case Nil => Nil
@@ -72,140 +242,53 @@ package object pretty {
     Doc(toChunks(s.toCharArray.toList))
   }
   def char(c: Char): Doc = text(c.toString)
-  def space = Doc(List(BreakingSpace))
-  def cr = Doc(List(CarriageReturn))
-  def blankline = Doc(List(BlankLine))
-
-/*
--- | A literal string.
-text :: String -> Doc
-text = Doc . toChunks
-  where toChunks :: String -> Seq D
-        toChunks [] = mempty
-        toChunks s = case break (=='\n') s of
-                          ([], _:ys) -> NewLine <| toChunks ys
-                          (xs, _:ys) -> Text (realLength xs) xs <|
-                                            (NewLine <| toChunks ys)
-                          (xs, [])      -> singleton $ Text (realLength xs) xs
-
--- | A character.
-char :: Char -> Doc
-char c = text [c]
-
--- | A breaking (reflowable) space.
-space :: Doc
-space = Doc $ singleton BreakingSpace
-
--- | A carriage return.  Does nothing if we're at the beginning of
--- a line; otherwise inserts a newline.
-cr :: Doc
-cr = Doc $ singleton CarriageReturn
-
--- | Inserts a blank line unless one exists already.
--- (@blankline <> blankline@ has the same effect as @blankline@.
--- If you want multiple blank lines, use @text "\\n\\n"@.
-blankline :: Doc
-blankline = Doc $ singleton BlankLine
-
--- | Uses the specified string as a prefix for every line of
--- the inside document (except the first, if not at the beginning
--- of the line).
-prefixed :: String -> Doc -> Doc
-prefixed pref doc = Doc $ singleton $ Prefixed pref doc
-
--- | Makes a 'Doc' flush against the left margin.
-flush :: Doc -> Doc
-flush doc = Doc $ singleton $ Flush doc
-
--- | Indents a 'Doc' by the specified number of spaces.
-nest :: Int -> Doc -> Doc
-nest ind = prefixed (replicate ind ' ')
-
--- | A hanging indent. @hang ind start doc@ prints @start@,
--- then @doc@, leaving an indent of @ind@ spaces on every
--- line but the first.
-hang :: Int -> Doc -> Doc -> Doc
-hang ind start doc = start <> nest ind doc
-
--- | @beforeNonBlank d@ conditionally includes @d@ unless it is
--- followed by blank space.
-beforeNonBlank :: Doc -> Doc
-beforeNonBlank d = Doc $ singleton (BeforeNonBlank d)
-
--- | Makes a 'Doc' non-reflowable.
-nowrap :: Doc -> Doc
-nowrap doc = Doc $ mapWithIndex replaceSpace $ unDoc doc
-  where replaceSpace _ BreakingSpace = Text 1 " "
-        replaceSpace _ x = x
-
--- | Returns the width of a 'Doc'.
-offset :: Doc -> Int
-offset d = case map realLength . lines . render Nothing $ d of
-                []    -> 0
-                os    -> maximum os
-
-block :: (String -> String) -> Int -> Doc -> Doc
-block filler width = Doc . singleton . Block width .
-                      map filler . chop width . render (Just width)
-
--- | @lblock n d@ is a block of width @n@ characters, with
--- text derived from @d@ and aligned to the left.
-lblock :: Int -> Doc -> Doc
-lblock = block id
-
--- | Like 'lblock' but aligned to the right.
-rblock :: Int -> Doc -> Doc
-rblock w = block (\s -> replicate (w - realLength s) ' ' ++ s) w
-
--- | Like 'lblock' but aligned centered.
-cblock :: Int -> Doc -> Doc
-cblock w = block (\s -> replicate ((w - realLength s) `div` 2) ' ' ++ s) w
-
--- | Returns the height of a block or other 'Doc'.
-height :: Doc -> Int
-height = length . lines . render Nothing
-
-chop :: Int -> String -> [String]
-chop _ [] = []
-chop n cs = case break (=='\n') cs of
-                  (xs, ys)     -> if len <= n
-                                     then case ys of
-                                             []     -> [xs]
-                                             (_:[]) -> [xs, ""]
-                                             (_:zs) -> xs : chop n zs
-                                     else take n xs : chop n (drop n xs ++ ys)
-                                   where len = realLength xs
-*/
-  
+  def space: Doc = Doc(List(BreakingSpace))
+  def cr: Doc = Doc(List(CarriageReturn))
+  def blankline: Doc = Doc(List(BlankLine))
+  def prefixed(prefix: String, doc: Doc): Doc = Doc(List(Prefixed(prefix, doc)))
+  def flush(doc: Doc): Doc = Doc(List(Flush(doc)))
+  def nest(indent: Int, doc: Doc): Doc = prefixed(" " * indent, doc)
+  def hang(indent: Int, start: Doc, doc: Doc): Doc = start <> nest(indent, doc)
+  def beforeNonBlank(doc: Doc): Doc = Doc(List(BeforeNonBlank(doc)))
+  def noWrap(doc: Doc): Doc = {
+    val unwrapped = doc.content map {(d: D) =>
+      d match {
+        case BreakingSpace => Text(1, " ")
+        case x => x
+      }
+    }
+    Doc(unwrapped)
+  }
+  def offset(doc: Doc): Int = render(None, doc).split('\n').toList.
+	  map((s: String) => realLength(s.toCharArray.toList)).foldLeft(0)((l: Int, r: Int) => math.max(l, r))
+  def block(filler: (String => String), width: Int)(doc: Doc): Doc = {
+    Doc(List(Block(width, chop(width, render(Some(width), doc)).map(filler(_)))))
+  }
+  def lblock(width: Int, doc: Doc): Doc = block((s: String) => s, width)(doc)
+  def rblock(width: Int, doc: Doc): Doc = {
+    block((s: String) => (" " * (width - realLength(s.toCharArray.toList))) + s, width)(doc)
+  }
+  def cblock(width: Int, doc: Doc): Doc = {
+    block((s: String) => (" " * ((width - realLength(s.toCharArray.toList)) / 2)) + s, width)(doc)
+  }
+  def height(doc: Doc): Int = render(None, doc).split('\n').length
+  def chop(width: Int, s: String): List[String] = {
+    if (s == "") Nil
+    else s.toCharArray.toList.span(_ != '\n') match {
+      case (xs, ys) if (realLength(xs) <= width) => ys match {
+        case Nil => List(xs.mkString)
+        case _ :: Nil => List(xs.mkString, "")
+        case _ :: zs => xs.mkString :: chop(width, zs.mkString)
+      }
+    }
+  }
+    
   def inside(start: Doc, end: Doc)(contents: Doc): Doc = start <> contents <> end
   def braces(doc: Doc): Doc = inside(char('{'), char('}'))(doc)
-  
-  
-/*
-inside :: Doc -> Doc -> Doc -> Doc
-inside start end contents =
-  start <> contents <> end
-
--- | Puts a 'Doc' in curly braces.
-braces :: Doc -> Doc
-braces = inside (char '{') (char '}')
-
--- | Puts a 'Doc' in square brackets.
-brackets :: Doc -> Doc
-brackets = inside (char '[') (char ']')
-
--- | Puts a 'Doc' in parentheses.
-parens :: Doc -> Doc
-parens = inside (char '(') (char ')')
-
--- | Wraps a 'Doc' in single quotes.
-quotes :: Doc -> Doc
-quotes = inside (char '\'') (char '\'')
-
--- | Wraps a 'Doc' in double quotes.
-doubleQuotes :: Doc -> Doc
-doubleQuotes = inside (char '"') (char '"')
-*/
+  def brackets(doc: Doc): Doc = inside(char('['), char(']'))(doc)
+  def parens(doc: Doc): Doc = inside(char('('), char(')'))(doc)
+  def quotes(doc: Doc): Doc = inside(char('\''), char('\''))(doc)
+  def doubleQuotes(doc: Doc): Doc = inside(char('"'), char('"'))(doc)
   
   def charWidth(c: Char): Int = {
     if (c < 0x0300) 1
