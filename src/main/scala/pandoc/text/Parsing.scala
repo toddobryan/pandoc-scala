cb8accab58ccd8c591cb9b533f1b69e9639c84f9
Todd O'Bryan <toddobryan@gmail.com>
2012-03-17 15:01:37 -0400

    really added Parsing this time

---
+++ src/main/scala/pandoc/text/Parsing.scala
@@ -0,0 +1,8 @@
+package pandoc.text
+import scala.util.parsing.combinator.RegexParsers
+
+object Parsing extends RegexParsers {
+  def emailChar: Parser[String] = regex("""[0-9a-zA-Z-+_.]"""r)
+  def emailAddress: Parser[(String, String)] = regex("""[0-9a-zA-Z]"""r) ~ emailChar * ~ literal("@")
+
+}
